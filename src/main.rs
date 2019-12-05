900fd373494377c220fe6909d8850dee4263a132
Faizan <fkaaa568@gmail.com>
2019-12-05 22:17:41 +0500

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("Hello, world!");
     println!("My Name is Faizan ali");
     println!("My father Name is Manzoor ali");
+    println!("My brother Name is Farhan Manzoor");
 }
